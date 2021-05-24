3ae851d122f338db42ec1db1bbe13403d2a06510
GuningDeng <s117661@ap.be>
2021-05-24 17:17:40 +0200

    hello-webfaster

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
+// let url = "http://localhost:3000/" // all API
+let url = "http://localhost:3000/match" // API with 'x'. 'x' can be modified by 'webfaster-app/routes/match'
+
+axios
+    .get(url + "/")   
+    .then(function(response) {
+        window.console.log(response);
+    })
+    .catch(function(error) {
+        window.console.log(error)
+    })
+
 
 createApp(App).mount('#app')
+
